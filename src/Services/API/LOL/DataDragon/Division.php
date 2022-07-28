716eae0272585dc10bee6aabe7cb03336d912305
zeggriim2 <lilian.dorazio@hotmail.fr>
2022-07-28 09:02:20 +0200

    Refactorisation

--- src/Services/API/LOL/DataDragon/Division.php
+++ src/Services/API/LOL/DataDragon/Division.php
@@ -4,10 +4,10 @@ namespace App\Services\API\LOL\DataDragon;
 
 class Division
 {
-    public const DIVISION_I = 'I';
-    public const DIVISION_II = 'II';
-    public const DIVISION_III = 'III';
-    public const DIVISION_IV = 'IV';
+    public const DIVISION_I     = 'I';
+    public const DIVISION_II    = 'II';
+    public const DIVISION_III   = 'III';
+    public const DIVISION_IV    = 'IV';
 
     public const ALL_DIVISION = [
         self::DIVISION_I,
--- src/Services/API/LOL/DataDragon/Platform.php
+++ src/Services/API/LOL/DataDragon/Platform.php
@@ -4,29 +4,31 @@ namespace App\Services\API\LOL\DataDragon;
 
 class Platform
 {
-    private const BR1 = 'br1.api.riotgames.com';
-    private const EUN1 = 'eun1.api.riotgames.com';
-    private const EUW1 = 'euw1.api.riotgames.com';
-    private const JP1 = 'jp1.api.riotgames.com';
-    private const KR = 'kr.api.riotgames.com';
-    private const LA1 = 'la1.api.riotgames.com';
-    private const LA2 = 'la2.api.riotgames.com';
-    private const NA1 = 'na1.api.riotgames.com';
-    private const OC1 = 'oc1.api.riotgames.com';
-    private const TR1 = 'tr1.api.riotgames.com';
-    private const RU = 'ru.api.riotgames.com';
+    private const URL   = '.api.riotgames.com';
+
+    private const BR1   = 'br1' . self::URL;
+    private const EUN1  = 'eun1' . self::URL;
+    private const EUW1  = 'euw1' . self::URL;
+    private const JP1   = 'jp1' . self::URL;
+    private const KR    = 'kr' . self::URL;
+    private const LA1   = 'la1' . self::URL;
+    private const LA2   = 'la2' . self::URL;
+    private const NA1   = 'na1' . self::URL;
+    private const OC1   = 'oc1' . self::URL;
+    private const TR1   = 'tr1' . self::URL;
+    private const RU    = 'ru' . self::URL;
 
     public const ALL_PLATFORM = [
-        'BR1' => self::BR1,
-        'EUN1' => self::EUN1,
-        'EUW1' => self::EUW1,
-        'JP1' => self::JP1,
-        'KR' => self::KR,
-        'LA1' => self::LA1,
-        'LA2' => self::LA2,
-        'NA1' => self::NA1,
-        'OC1' => self::OC1,
-        'TR1' => self::TR1,
-        'RU' => self::RU,
+        'BR1'   => self::BR1,
+        'EUN1'  => self::EUN1,
+        'EUW1'  => self::EUW1,
+        'JP1'   => self::JP1,
+        'KR'    => self::KR,
+        'LA1'   => self::LA1,
+        'LA2'   => self::LA2,
+        'NA1'   => self::NA1,
+        'OC1'   => self::OC1,
+        'TR1'   => self::TR1,
+        'RU'    => self::RU,
     ];
 }
