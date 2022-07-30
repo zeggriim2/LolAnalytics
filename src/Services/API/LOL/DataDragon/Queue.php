1f08ce8f594d2dc761ba896b765a63d590c68dff
zeggriim2 <lilian.dorazio@hotmail.fr>
2022-07-31 01:40:56 +0200

    Enum : - Platfom - Queue - Region - Division - Tier

--- src/Services/API/LOL/DataDragon/Queue.php
+++ src/Services/API/LOL/DataDragon/Queue.php
@@ -2,10 +2,17 @@
 
 namespace App\Services\API\LOL\DataDragon;
 
-class Queue
+use Fresh\DoctrineEnumBundle\DBAL\Types\AbstractEnumType;
+
+class Queue extends AbstractEnumType
 {
     public const RANKED_SOLO = 'RANKED_SOLO_5x5';
     public const RANKED_FLEX = 'RANKED_FLEX_SR';
+
+    protected static array $choices = [
+        self::RANKED_SOLO => self::RANKED_SOLO,
+        self::RANKED_FLEX => self::RANKED_FLEX,
+    ];
 
     public const ALL_QEUEUES = [
         self::RANKED_SOLO,
