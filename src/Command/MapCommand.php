e16f4b3152ec7e99ee144221ff42e628694595d6
zeggriim2 <lilian.dorazio@hotmail.fr>
2022-08-01 08:58:08 +0200

    Maps + Command

---
+++ src/Command/MapCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Map;
+use App\Entity\Version;
+use App\Services\API\LOL\DataDragon\DataDragonApi;
+use App\Services\API\LOL\DataDragon\GeneralApi;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:maps',
+    description: 'Ajout les Maps en BDD',
+)]
+class MapCommand extends Command
+{
+    public function __construct(
+        private GeneralApi $generalApi,
+        private EntityManagerInterface $doctrine,
+        string $name = null
+    ) {
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $maps = $this->generalApi->getMaps();
+
+        $countMaps = 0;
+        foreach ($maps as $map) {
+            $mapRepo = $this->doctrine->getRepository(Map::class)->findOneBy(['mapId' => $map['mapId']]);
+            if (null === $mapRepo) {
+                $mapEntity = (new Map())
+                    ->setMapId($map['mapId'])
+                    ->setName($map['mapName'])
+                    ->setNotes($map['notes'])
+                ;
+                $this->doctrine->persist($mapEntity);
+                ++$countMaps;
+            }
+        }
+        $this->doctrine->flush();
+
+        if (0 === $countMaps) {
+            $phrase = "Aucun élément de map n'a été ajouté.";
+        } else {
+            $phrase = "$countMaps map ont été ajouté en bdd.";
+        }
+
+        $io->success($phrase);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Entity/Map.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MapRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+#[ORM\Entity(repositoryClass: MapRepository::class)]
+class Map
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $mapId;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $notes;
+
+    #[Gedmo\Timestampable(on: 'create')]
+    #[ORM\Column(name: 'created', type: 'datetime_immutable')]
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMapId(): ?int
+    {
+        return $this->mapId;
+    }
+
+    public function setMapId(int $mapId): self
+    {
+        $this->mapId = $mapId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(string $notes): self
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
