36f83fd42da16d60a51485bdf98b3e3272b9e33e
zeggriim2 <lilian.dorazio@hotmail.fr>
2022-09-09 09:33:00 +0200

    Clean Code + Update Package

--- src/Controller/StatController.php
+++ src/Controller/StatController.php
@@ -29,7 +29,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StatController extends AbstractController
+final class StatController extends AbstractController
 {
     public function __construct(
         private ManagerRegistry $doctrine
