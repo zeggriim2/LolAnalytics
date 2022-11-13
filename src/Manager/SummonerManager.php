17c7811a4cc3a3052fbe9cef9ed08c57b4b632bb
zeggriim2 <lilian.dorazio@hotmail.fr>
2022-11-13 23:49:35 +0100

    Summoner Refacto

--- src/Manager/SummonerManager.php
+++ src/Manager/SummonerManager.php
@@ -15,17 +15,23 @@ class SummonerManager
 
     /**
      * @return string|void
+     * @throws \Exception
      */
     public function enregistrer(?SummonerDTO $summonerApi)
     {
         if (null === $summonerApi) {
             // TODO Exception
+//            throw new \Exception("Nom d\'utilisateur introuvable");
             return $error['message'] = 'Nom d\'utilisateur introuvable';
         } elseif ($this->doctrine->getRepository(Invocateur::class)->findOneBy(['idLol' => $summonerApi->getId()])) {
             // TODO Exception
             return $error['message'] = 'Nom d\'utilisateur déjà existant';
         }
 
+        $this->create($summonerApi);
+    }
+
+    public function create(SummonerDTO $summonerApi){
         $invocateur = (new Invocateur())
             ->setName($summonerApi->getName())
             ->setIdLol($summonerApi->getId())
