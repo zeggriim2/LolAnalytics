7d461ec6b7c41faa90ea31d8c4cf7ebef3fe5e18
Zeggriim <zeggriim@gmail.com>
2026-03-06 15:46:50 +0100

    chore(test): accelère les tests functionnel

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
